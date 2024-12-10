72fec7583026d547f7dd4a23a4b506fa1be749ad
Finn Gilligan <finngilligan0@gmail.com>
2024-12-10 15:13:24 -0500

    fixed leak

--- NewsAPI/ContentView.swift
+++ NewsAPI/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
             Text(json)
         }
         .task {
-            let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&category=science&q=space&apiKey=9ec3e24bf7e24b4392201d0a539829b9")!
+            let url = URL(string: "")!
                 let (data, _) = try! await URLSession.shared.data(from: url)
                 json = String(decoding: data, as: UTF8.self)
         }
